a19395bcb1b76e2d9342191aa9b5aaad09de0be9
Xita <yuan45513@163.com>
2021-03-25 15:47:26 +0800

    样式初始化、ant引入

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { HashRouter, Switch, Route } from 'react-router-dom'
+import Home from './views/Home'
+import About from "./views/About"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return (
+      <div>
+        <HashRouter>
+          <Switch>
+            <Route exact component={Home} path="/"></Route>
+            <Route component={About} path="/about"></Route>
+            <Route></Route>
+          </Switch>
+        </HashRouter>
+      </div>
+
+    )
+  }
 }
+
 
 export default App;
