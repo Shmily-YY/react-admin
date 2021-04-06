d6949936219bb3b204e18b8a2b6bacfa1224d94e
Xita <yuan45513@163.com>
2021-04-06 10:36:42 +0800

    登录验证逻辑

--- src/api/account.js
+++ src/api/account.js
@@ -1,9 +1,23 @@
 import service from '../utils/request'
 
-// 登录
+/**
+ * 登录
+ */
 export function Login(data) {
   return service.request({
     url: "/login",
+    method: "post",
+    data
+  })
+}
+
+/**
+ * 验证码
+ */
+
+export function GetCode(data) {
+  return service.request({
+    url: "/getSms",
     method: "post",
     data
   })
--- src/views/login/LoginForm.js
+++ src/views/login/LoginForm.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react'
 // css
 import './index.scss'
 
-import { Form, Input, Button, Row, Col } from 'antd';
+import { Form, Input, Button, Row, Col, message } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 // 验证
-import { validate_password } from '../../utils/validate'
+import { validate_password, validate_email } from '../../utils/validate'
 // API
-import { Login } from '../../api/account'
+import { Login, GetCode } from '../../api/account'
 
 class LoginForm extends Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      username: "409019683@qq.com",
+      code_button_disabled: false,
+      code_button_loading: false,
+      code_button_text: "获取验证码"
+    }
+    // react 没有数据双向绑定的概念
   }
-
+  // 登录
   onFinish = (values) => {
     console.log('Received values of form: ', values);
     Login().then(res => {
@@ -23,10 +29,71 @@ class LoginForm extends Component {
       console.log(err)
     })
   };
+  // 获取验证码
+  getCode = () => {
+    this.setState({
+      code_button_loading: true,
+      code_button_text: "发送中"
+    })
+    if (!this.state.username) {
+      message.warning('用户名不能为空！！！', 2);
+      return false
+    }
+    const reqObj = {
+      username: this.state.username,
+      module: "login"
+    }
+    GetCode(reqObj).then(res => {
+      // 执行倒计时
+      console.log(res)
+      this.setState({
+        code_button_loading: false
+      })
+      this.countDown()
+    }).catch(err => {
+      this.setState({
+        code_button_loading: false,
+        code_button_text: "重新获取"
+      })
+    })
+  }
+  /**倒计时 */
+  countDown = () => {
+    let timer = null
+    let sec = 60
+    this.setState({
+      code_button_loading: false,
+      code_button_disabled: true,
+      code_button_text: `${sec}s`
+    })
+    /**定时器 */
+    timer = setInterval(() => {
+      sec--
+      if (sec <= 0) {
+        this.setState({
+          code_button_text: "重新获取",
+          code_button_disabled: false,
+        })
+        clearInterval(timer)
+        return false
+      }
+      this.setState({
+        code_button_text: `${sec}s`
+      })
+    }, 1000);
+
+  }
+  inputChange = (e) => {
+    this.setState({
+      username: e.target.value
+    })
+  }
   toggleForm = () => {
     this.props.switchForm('register')
   }
   render() {
+    const { username, code_button_disabled, code_button_loading, code_button_text } = this.state
+    const _this = this
     return (
       <div>
         <div className="form-header">
@@ -44,10 +111,22 @@ class LoginForm extends Component {
               name="username"
               rules={[
                 { required: true, message: '邮箱不能为空' },
-                { type: "email", message: "邮箱格式不正确" }
+                // { type: "email", message: "邮箱格式不正确" }
+                ({ getFieldValue }) => ({
+                  validator(rule, value) {
+                    console.log("11111111", validate_email(value))
+                    if (validate_email(value)) {
+                      _this.setState({
+                        code_button_disabled: false
+                      })
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(new Error('邮箱格式不正确！'));
+                  },
+                })
               ]}
             >
-              <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="请输入邮箱" />
+              <Input value={username} onChange={this.inputChange} prefix={<UserOutlined className="site-form-item-icon" />} placeholder="请输入邮箱" />
             </Form.Item>
 
             <Form.Item
@@ -78,6 +157,7 @@ class LoginForm extends Component {
               rules={[
                 { required: true, message: '验证码不能为空' },
                 { len: 6, message: '请输入长度为6位的验证码' },
+
               ]}
             >
               <Row gutter={13}>
@@ -85,7 +165,7 @@ class LoginForm extends Component {
                   <Input prefix={<LockOutlined className="site-form-item-icon" />} placeholder="请输入验证码" />
                 </Col>
                 <Col span={9}>
-                  <Button type="danger" block>获取验证码</Button>
+                  <Button loading={code_button_loading} disabled={code_button_disabled} type="danger" block onClick={this.getCode}>{code_button_text}</Button>
                 </Col>
               </Row>
             </Form.Item>
